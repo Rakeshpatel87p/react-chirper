c6da9b27896511719f761ceaa14ce05be8379bf9
rakesh.patel <rpatel@arke.com>
2018-12-27 09:54:32 -0500

    subscribe to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
 
@@ -10,11 +11,21 @@ class App extends Component {
 
 	render() {
     	return (
-	      <div>
-	        Hi Rakesh, looking dashing today.
-	      </div>
+    		<div>
+    			Starter Code
+    			{ this.props.loading === true 
+    				? null
+    				: <Dashboard />
+    			}
+    		</div>
     	)
   	}
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+	return {
+		loading: authedUser === null
+	}
+}
+
+export default connect(mapStateToProps)(App)
